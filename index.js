49b3c76631adfad25de0047cd0b8745d80f755f1
cailinshaffer <cailinshaffer@gmail.com>
2023-01-05 10:44:42 -0700

    adding pets model to database

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 // require packages
 require('dotenv').config()
 const express = require('express')
+//const request = require('request')
 const cookieParser = require('cookie-parser')
 const db = require('./models')
 const crypto = require('crypto-js')
+const axios = require('axios') 
+const fs = require('fs')
 
 //app config
 const app = express()
@@ -13,6 +16,10 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: false}))
 //tell express to parse incoming cookies
 app.use(cookieParser())
+app.set('view engine', 'ejs')
+const CLIENT_ID = process.env.CLIENT_ID
+const SECRET_ID = process.env.SECRET_ID
+const petUrl = "https://api.petfinder.com/v2/animals"
 
 //custome auth middleware that checks the cookies for a user id
 //and it finds one, look up the user in the db
@@ -63,6 +70,195 @@ app.get('/', (req,res) => {
 })
 
 app.use('/users', require('./controllers/users'))
+
+
+// async function fetchPets() {
+//     try {
+//         const body = {
+//             'grant_type': 'client_credentials',
+//             'client_id': CLIENT_ID,
+//             'client_secret': SECRET_ID
+
+//         }
+
+//         // https://api.petfinder.com/v2/oauth2/token
+//         const tokenUrl = 'https://api.petfinder.com/v2/oauth2/token'
+//         const tokenResponse = await axios.post(tokenUrl, body)
+//         console.log('bearer token reponse:', tokenResponse.data)
+
+//         const options = {
+//             headers: {
+//                 Authorization: `Bearer ${tokenResponse.data.access_token}`
+//             }
+//         }
+
+//         const dataResponse = await axios.get('https://api.petfinder.com/v2/animals?type=dog&page=2', options)
+
+//         console.log('data response:', dataResponse.data)
+//     } catch (err) {
+//         console.log(err)
+//     }
+// }
+// fetchPets()
+
+
+
+// routes for retrieving animals
+
+//  GET animal  GET https://api.petfinder.com/v2/animals
+// app.get('/animals', (req, res) => {
+//     res.send("animals here")
+// })
+
+
+
+
+app.get('/pets', async (req,res) => {
+    try{
+        // const animals = fs.readFileSync('/animals')
+        // let animalData = JSON.parse(animals)
+        // let animalUrl = 'https://api.petfinder.com/v2/animals'
+        // axios.get(animalUrl).then(response => {
+        //     //let animal = apiResponse.data.results
+        //     res.send(response.data)
+        // })
+       
+        const body = {
+            'grant_type': 'client_credentials',
+            'client_id': CLIENT_ID,
+            'client_secret': SECRET_ID
+    
+        }
+    
+        // https://api.petfinder.com/v2/oauth2/token
+        const tokenUrl = 'https://api.petfinder.com/v2/oauth2/token'
+        const tokenResponse = await axios.post(tokenUrl, body)
+        console.log('bearer token reponse:', tokenResponse.data)
+    
+        const options = {
+            headers: {
+                Authorization: `Bearer ${tokenResponse.data.access_token}`
+            }
+        } 
+    
+    
+    const dataResponse = await axios.get('https://api.petfinder.com/v2/animals', options)
+       res.send(dataResponse.data)
+    //    res.render('pets/search.ejs')
+    }catch(err){
+        console.log(err)
+    }
+})
+
+
+
+// GET all 
+app.get('/pets/search', async (req, res) => {
+    try {
+       
+       
+        const body = {
+            'grant_type': 'client_credentials',
+            'client_id': CLIENT_ID,
+            'client_secret': SECRET_ID
+    
+        }
+    
+        // https://api.petfinder.com/v2/oauth2/token
+        const tokenUrl = 'https://api.petfinder.com/v2/oauth2/token'
+        const tokenResponse = await axios.post(tokenUrl, body)
+        console.log('bearer token reponse:', tokenResponse.data)
+    
+        const options = {
+            headers: {
+                Authorization: `Bearer ${tokenResponse.data.access_token}`
+            }
+        } 
+    
+    const dataResponse = await axios.get('https://api.petfinder.com/v2/animals', options)
+    //console.log(`incoming request: ${req.method} - ${req.url}`)
+      res.render('pets/search.ejs')
+    //res.send(dataResponse.data)
+    }catch(err){
+        console.log(err)
+
+         //fetchPets()
+    }
+   res.render('pets/search.ejs')
+    
+    //res.send("search animals")
+})
+
+
+
+// app.get('/animails', async (req, res) => {
+//     const body = {
+//         'grant_type': 'client_credentials',
+//         'client_id': CLIENT_ID,
+//         'client_secret': SECRET_ID
+
+//     }
+
+//     // https://api.petfinder.com/v2/oauth2/token
+//     const tokenUrl = 'https://api.petfinder.com/v2/oauth2/token'
+//     const tokenResponse = await axios.post(tokenUrl, body)
+//     console.log('bearer token reponse:', tokenResponse.data)
+
+//     const options = {
+//         headers: {
+//             Authorization: `Bearer ${tokenResponse.data.access_token}`
+//         }
+//     }
+
+//     // const dataResponse = await axios.get('https://api.petfinder.com/v2/animals?type=dog&page=2', options)
+//     // let qs = {
+//     //   s: 'animals',
+//     //   access_token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJmamZrdzF1T1RjdWFVT0ljc2llTHFPbVcycWNVOE5sQjQ4OE5Edk1xODZRdDNtMTVURSIsImp0aSI6ImYyZTVhMWM1MzNiNGU5ODc2ODdkOWY1OTJlN2Q4Y2ZjODA3ZTkzOWMwYzJlMDRiZmRhZmU5Y2IzMWQ1ODRlODk1ZjI2ODNmMmYzOTZiMWMxIiwiaWF0IjoxNjcyNzcxNjA2LCJuYmYiOjE2NzI3NzE2MDYsImV4cCI6MTY3Mjc3NTIwNiwic3ViIjoiIiwic2NvcGVzIjpbXX0.zOFa14wsoAnPbV7wNZRvDodCrQtNs89UMqbKUb-HnNSBhS1wvxuiVr6oMWs7pm1rN6dqJek78xuMLmJwDq0bdT75dxh4P3kbTr4BKP15RzOxW7Jjcsl9K3xR4OyaMmdh1cWI6QLK9yh8SgVlF7VyC7uqPhLWnlE54d5rnVsDbdSbOPgWFjG0A3QWwigNSUHEjH5DlKQ0xGLQPGw4nROEe3P6Cs5wiNWISPaw5Z_O4ikiTpM7cLbymknYeQiAzw86XB4-dZnUAYbHnFWBsaCFf1WPZvXSojINvybKe9A2J1iLzcGb5AHIlu_gDN7ctAVU2A2rzqrH66My6FxqKepIuA'
+//     // };
+  
+//     // request({
+//     //   url: 'https://api.petfinder.com/v2/animals',
+//     //   qs: qs
+//     // }, function (error, response, body) {
+//     //   if (!error && response.statusCode == 200) {
+//     //     var dataObj = JSON.parse(body);
+//     //     res.send(dataObj.Search);
+//     //   }
+//     // });
+//   });
+
+
+
+
+
+// app.get('/animals', async (req,res) => {
+//     const body = {
+//         'grant_type': 'client_credentials',
+//         'client_id': CLIENT_ID,
+//         'client_secret': SECRET_ID
+
+//     }
+
+//     // https://api.petfinder.com/v2/oauth2/token
+//     const tokenUrl = 'https://api.petfinder.com/v2/oauth2/token'
+//     const tokenResponse = await axios.post(tokenUrl, body)
+//     console.log('bearer token reponse:', tokenResponse.data)
+
+//     const options = {
+//         headers: {
+//             Authorization: `Bearer ${tokenResponse.data.access_token}`
+//         }
+//     }
+
+//     const dataResponse = await axios.get('https://api.petfinder.com/v2/animals?type=dog&page=2', options)
+   
+   
+//     let animalUrl = 'https://api.petfinder.com/v2/animals'
+//     axios.get(animalUrl).then(apiResponse => {
+//         let animal = apiResponse.data.results
+//         res.render(animal)
+//     })
+// })
 
 
 //listen on port
---
+++ models/pets.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class pets extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  pets.init({
+    type: DataTypes.STRING,
+    age: DataTypes.INTEGER,
+    breed: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'pets',
+  });
+  return pets;
+};
